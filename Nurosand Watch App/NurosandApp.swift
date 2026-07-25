ec82d26875294dec8a3e663aa6cbb8dbae769036
mattcc18 <corcoran.matthew18@gmail.com>
2026-07-25 15:54:57 +0100

    Initial Commit

---
+++ Nurosand Watch App/NurosandApp.swift
@@ -0,0 +1,17 @@
+//
+//  NurosandApp.swift
+//  Nurosand Watch App
+//
+//  Created by Matthew Corcoran on 25/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct Nurosand_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
